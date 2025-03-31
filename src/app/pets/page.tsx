162c386941b06732f8df0bc92120d555ba2d7147
Matheus Morete Espinoso <matheus.moorete@gmail.com>
2025-03-31 11:03:51 -0300

    Remove listas de nomes não utilizadas no arquivo pets/page.tsx

--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -5,104 +5,7 @@ import NomeDisplay from '@/components/NomeDisplay';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import AINameGenerator from '@/components/AINameGenerator';
-
-// Lista de nomes de pets separados por gênero
-const nomesCachorro = {
-  macho: [
-    'Thor', 'Max', 'Rex', 'Bob', 'Duke', 'Toby', 'Fred', 'Simba', 
-    'Billy', 'Luke', 'Rocky', 'Buddy', 'Apollo', 'Zeus', 'Bruno',
-    'Spike', 'Marley', 'Lucky', 'Charlie', 'Theo'
-  ],
-  femea: [
-    'Luna', 'Bella', 'Mel', 'Nina', 'Amora', 'Princesa', 'Lola', 
-    'Meg', 'Mia', 'Kiara', 'Nina', 'Nala', 'Pandora', 'Laika', 'Malu',
-    'Maya', 'Kika', 'Pituca', 'Pepper', 'Penny'
-  ],
-  neutro: [
-    'Thor', 'Luna', 'Max', 'Bella', 'Rex', 'Mel', 'Bob', 'Nina', 
-    'Duke', 'Amora', 'Toby', 'Princesa', 'Fred', 'Lola', 'Simba', 
-    'Meg', 'Billy', 'Mia', 'Luke', 'Kiara'
-  ]
-};
-
-const nomesGato = {
-  macho: [
-    'Oliver', 'Leo', 'Simba', 'Tom', 'Félix', 'Garfield', 'Thor', 
-    'Bóris', 'Ozzy', 'Mingau', 'Romeu', 'Bigode', 'Tigre', 'Caju',
-    'Merlim', 'Nescau', 'Logan', 'Toddy', 'Trovão', 'Miau'
-  ],
-  femea: [
-    'Luna', 'Lili', 'Nina', 'Mia', 'Mely', 'Nala', 'Mimi', 
-    'Chloe', 'Amora', 'Mel', 'Cacau', 'Malu', 'Jade', 'Filó',
-    'Cherie', 'Pandora', 'Maggie', 'Pipoca', 'Pantera', 'Sofia'
-  ],
-  neutro: [
-    'Oliver', 'Luna', 'Leo', 'Lili', 'Simba', 'Nina', 'Tom', 'Mia', 
-    'Félix', 'Mely', 'Garfield', 'Nala', 'Thor', 'Mimi', 'Frajola', 
-    'Chloe', 'Bóris', 'Amora', 'Ozzy', 'Mel'
-  ]
-};
-
-const nomesPeixe = {
-  macho: [
-    'Nemo', 'Flash', 'Splash', 'Marlin', 'Finn', 'Gil', 'Blue',
-    'Bolha', 'Aquaman', 'Tritão', 'Marin', 'Coral', 'Oceano', 'Shark',
-    'Flipper', 'Nado', 'Azulão', 'Peixoto', 'Vermelho', 'Limão'
-  ],
-  femea: [
-    'Dory', 'Ariel', 'Coral', 'Pearl', 'Ruby', 'Aqua', 'Wanda',
-    'Sereia', 'Bolha', 'Bubble', 'Marina', 'Goldie', 'Pérola', 'Azulina',
-    'Escama', 'Cristal', 'Nada', 'Luna', 'Estrela', 'Água'
-  ],
-  neutro: [
-    'Nemo', 'Dory', 'Bolha', 'Azul', 'Flash', 'Splash', 'Coral', 'Marlin', 
-    'Goldie', 'Bubble', 'Finn', 'Ariel', 'Gil', 'Aqua', 'Wanda', 
-    'Flipper', 'Nado', 'Blue', 'Pearl', 'Ruby'
-  ]
-};
-
-const nomesCoelho = {
-  macho: [
-    'Pernalonga', 'Thumper', 'Orelhudo', 'Tambor', 'Saltitante', 'Puff',
-    'Algodão', 'Floco', 'Macio', 'Salto', 'Pipinho', 'Bolinha', 'Pintado',
-    'Felpudo', 'Chocolate', 'Cenoura', 'Branquinho', 'Pipoca', 'Nevado', 'Pulão'
-  ],
-  femea: [
-    'Fofa', 'Fofinha', 'Bolinha', 'Pipoca', 'Algodão', 'Neve', 'Cenoura', 
-    'Patinha', 'Floquinho', 'Pérola', 'Branquinha', 'Pula-pula', 'Orelhinha',
-    'Lola', 'Lili', 'Cotton', 'Belinha', 'Fru-fru', 'Moranguinho', 'Boneca'
-  ],
-  neutro: [
-    'Pernalonga', 'Fofinho', 'Thumper', 'Orelhudo', 'Algodão', 'Cenoura', 
-    'Pipoca', 'Floco', 'Macio', 'Salto', 'Pipinho', 'Biju', 'Tambor', 
-    'Bolinha', 'Felpudo', 'Pompom', 'Chocolate', 'Branquinho', 'Patinha', 'Neve'
-  ]
-};
-
-// Versões reduzidas para os outros animais
-const nomesRoedor = {
-  macho: ['Twister', 'Pingo', 'Bolt', 'Mickey', 'Stuart', 'Nugget', 'Amendoim', 'Pitufo', 'Cookie', 'Caramelo'],
-  femea: ['Pipoca', 'Minnie', 'Lilo', 'Bolinha', 'Floco', 'Petunia', 'Mochi', 'Lentilha', 'Pimenta', 'Fofinha'],
-  neutro: ['Twister', 'Pipoca', 'Pingo', 'Bolt', 'Minnie', 'Mickey', 'Lilo', 'Bolinha', 'Floco', 'Pitufo']
-};
-
-const nomesAve = {
-  macho: ['Blu', 'Zazu', 'Louro', 'Flash', 'Sol', 'Bob', 'Kiwi', 'Sky', 'Rio', 'Azulão'],
-  femea: ['Jade', 'Safira', 'Piu-Piu', 'Petúnia', 'Cecília', 'Mel', 'Coco', 'Rainbow', 'Sunny', 'Esmeralda'],
-  neutro: ['Blu', 'Zazu', 'Louro', 'Piu-Piu', 'Sky', 'Jade', 'Safira', 'Rio', 'Flash', 'Sol']
-};
-
-const nomesReptil = {
-  macho: ['Draco', 'Spike', 'Rex', 'Godzilla', 'Yoshi', 'Rango', 'Crush', 'Ziggy', 'Komodo', 'Slither'],
-  femea: ['Serpente', 'Hydra', 'Scale', 'Cobra', 'Lagartixa', 'Nila', 'Escama', 'Reptilia', 'Verde', 'Iguana'],
-  neutro: ['Draco', 'Spike', 'Rex', 'Godzilla', 'Yoshi', 'Rango', 'Crush', 'Flash', 'Ziggy', 'Komodo']
-};
-
-const nomesExotico = {
-  macho: ['Exótico', 'Rajah', 'Django', 'Yoda', 'Bamboo', 'Storm', 'Enigma', 'Cosmos', 'Ozzy', 'Onyx'],
-  femea: ['Safira', 'Íris', 'Quimera', 'Nebulosa', 'Pandora', 'Aurora', 'Winter', 'Mystic', 'Shadow', 'Quest'],
-  neutro: ['Exótico', 'Safira', 'Rajah', 'Íris', 'Quimera', 'Nebulosa', 'Pandora', 'Django', 'Yoda', 'Aurora']
-};
+import { LISTA_NOMES_PETS } from '@/data/petNames';
 
 export default function Pets() {
   const [tipoAnimal, setTipoAnimal] = useState('cachorro');
@@ -120,31 +23,31 @@ export default function Pets() {
       
       switch(tipoAnimal) {
         case 'cachorro':
-          listaNomes = nomesCachorro[generoAnimal as keyof typeof nomesCachorro];
+          listaNomes = LISTA_NOMES_PETS.cachorro[generoAnimal as keyof typeof LISTA_NOMES_PETS.cachorro];
           break;
         case 'gato':
-          listaNomes = nomesGato[generoAnimal as keyof typeof nomesGato];
+          listaNomes = LISTA_NOMES_PETS.gato[generoAnimal as keyof typeof LISTA_NOMES_PETS.gato];
           break;
         case 'peixe':
-          listaNomes = nomesPeixe[generoAnimal as keyof typeof nomesPeixe];
+          listaNomes = LISTA_NOMES_PETS.peixe[generoAnimal as keyof typeof LISTA_NOMES_PETS.peixe];
           break;
         case 'coelho':
-          listaNomes = nomesCoelho[generoAnimal as keyof typeof nomesCoelho];
+          listaNomes = LISTA_NOMES_PETS.coelho[generoAnimal as keyof typeof LISTA_NOMES_PETS.coelho];
           break;
         case 'roedor':
-          listaNomes = nomesRoedor[generoAnimal as keyof typeof nomesRoedor];
+          listaNomes = LISTA_NOMES_PETS.roedor[generoAnimal as keyof typeof LISTA_NOMES_PETS.roedor];
           break;
         case 'ave':
-          listaNomes = nomesAve[generoAnimal as keyof typeof nomesAve];
+          listaNomes = LISTA_NOMES_PETS.ave[generoAnimal as keyof typeof LISTA_NOMES_PETS.ave];
           break;
         case 'reptil':
-          listaNomes = nomesReptil[generoAnimal as keyof typeof nomesReptil];
+          listaNomes = LISTA_NOMES_PETS.reptil[generoAnimal as keyof typeof LISTA_NOMES_PETS.reptil];
           break;
         case 'exotico':
-          listaNomes = nomesExotico[generoAnimal as keyof typeof nomesExotico];
+          listaNomes = LISTA_NOMES_PETS.exotico[generoAnimal as keyof typeof LISTA_NOMES_PETS.exotico];
           break;
         default:
-          listaNomes = nomesCachorro.neutro;
+          listaNomes = LISTA_NOMES_PETS.cachorro.neutro;
       }
       
       const indiceAleatorio = Math.floor(Math.random() * listaNomes.length);
